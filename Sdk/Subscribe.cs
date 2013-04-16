97db0924902be5b9ec5555a31ceded3d9a4bdd12
Jim Lenox <jlenox@gmail.com>
2013-04-16 17:02:40 -0500

    Added ability to subscibe to site mail list

---
+++ Sdk/Subscribe.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BringLocal.Sdk
+{
+    public class Subscribe
+    {
+        [JsonProperty("publisherId")]
+        public Guid PublisherId { get; set; }
+        [JsonProperty("siteId")]
+        public Guid SiteId { get; set; }
+        [JsonProperty("emailAddress")]
+        public string EmailAddress { get; set; }
+        [JsonProperty("source")]
+        public string Source { get; set; }
+
+        public Task<Subscription> Submit()
+        {
+            var request = ClientHelper.Request("subscribe", Method.POST);
+            var json = JsonConvert.SerializeObject(this);
+            request.AddParameter("text/json", JsonConvert.SerializeObject(this), ParameterType.RequestBody);
+
+            var tcs = new TaskCompletionSource<Subscription>();
+            ClientHelper.Client().ExecuteAsync(request, response =>
+            {
+                if (response.ErrorException == null)
+                {
+                    tcs.SetResult(new Subscription(response));
+                }
+                else
+                {
+                    tcs.SetException(response.ErrorException);
+                }
+            });
+            return tcs.Task;
+        }
+    }
+}
---
+++ Sdk/Subscription.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System.Net;
+
+namespace BringLocal.Sdk
+{
+    public class Subscription : ApiResponse
+    {
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+        [JsonProperty("message")]
+        public string Message { get; set; }
+        [JsonProperty("code")]
+        public string Code { get; set; }
+        [JsonProperty("originalMessage")]
+        public string OriginalMessage { get; set; }
+        [JsonProperty("doubleOptIn")]
+        public bool DoubleOptIn { get; set; }
+
+        public Subscription()
+        {
+            
+        }
+        public Subscription(IRestResponse response)
+        {
+            StatusCode = response.StatusCode;
+
+            switch (StatusCode)
+            {
+                case HttpStatusCode.OK:
+                case HttpStatusCode.Created:
+                    JsonConvert.PopulateObject(response.Content, this);
+                    break;
+                case HttpStatusCode.NoContent:
+                    //NOP: nothing to deserialize
+                    break;
+                default:
+                    DeserializeErrors(response.Content);
+                    break;
+            }
+        }
+    }
+}
