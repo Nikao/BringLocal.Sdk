0088ce86ba17ce4e814f4ce36e7515d49995953b
Jim Lenox <jlenox@gmail.com>
2013-04-15 21:40:32 -0500

    Added Signout to User

--- Sdk/User.cs
+++ Sdk/User.cs
@@ -1,4 +1,6 @@
-﻿using RestSharp;
+﻿using System.Net;
+using Newtonsoft.Json;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +9,13 @@ using System.Threading.Tasks;
 
 namespace BringLocal.Sdk
 {
-    public class User
+    public class User : ApiResponse
     {
+        public User(IRestResponse response)
+        {
+            this.StatusCode = response.StatusCode;
+
+        }
 
         public static Task<UserToken> Authenticate(string userName, string password)
         {
@@ -30,6 +37,27 @@ namespace BringLocal.Sdk
                 }
             });
             return tcs.Task;
+        }
+
+        public static Task<HttpStatusCode> SignOut(string token)
+        {
+            var request = ClientHelper.Request("users/signout", Method.DELETE);
+            request.AddUrlSegment("token", token);
+
+            var tcs = new TaskCompletionSource<HttpStatusCode>();
+            ClientHelper.Client().ExecuteAsync(request, response =>
+            {
+                if (response.ErrorException == null)
+                {
+                    tcs.SetResult(response.StatusCode);
+                }
+                else
+                {
+                    tcs.SetException(response.ErrorException);
+                }
+            });
+            return tcs.Task;
+
         }
     }
 }
